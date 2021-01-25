d4117a98dc21da66b44c5e49d2394081da44f0b1
Kyle Bensink <kyle.r.bensink.21@dartmouth.edu>
2021-01-25 16:12:04 -0500

    commenting CORS stuff out

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,14 @@ import sys
 import numpy
 from sklearn.linear_model import LinearRegression
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 import json
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    CORS(app)
-    app.config['CORS_HEADERS'] = 'Content-Type'
+    # CORS(app)
+    # app.config['CORS_HEADERS'] = 'Content-Type'
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
