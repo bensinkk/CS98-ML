33964bc8b846f1b42583d0e3270fb341c554939c
Kyle Bensink <kyle.r.bensink.21@dartmouth.edu>
2021-01-25 16:30:36 -0500

    trying dummy code

--- app.py
+++ app.py
@@ -1,60 +1,113 @@
-import os
-import pickle
-import pandas as pd
-import sys
-import numpy
-from sklearn.linear_model import LinearRegression
-from flask import Flask, request, jsonify
-# from flask_cors import CORS, cross_origin
-import json
+# import os
+# import pickle
+# import pandas as pd
+# import sys
+# import numpy
+# from sklearn.linear_model import LinearRegression
+# from flask import Flask, request, jsonify
+# # from flask_cors import CORS, cross_origin
+# import json
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    # CORS(app)
-    # app.config['CORS_HEADERS'] = 'Content-Type'
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# def create_app(test_config=None):
+#     # create and configure the app
+#     app = Flask(__name__, instance_relative_config=True)
+#     # CORS(app)
+#     # app.config['CORS_HEADERS'] = 'Content-Type'
+#     app.config.from_mapping(
+#         SECRET_KEY='dev',
+#         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+#     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+#     if test_config is None:
+#         # load the instance config, if it exists, when not testing
+#         app.config.from_pyfile('config.py', silent=True)
+#     else:
+#         # load the test config if passed in
+#         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+#     # ensure the instance folder exists
+#     try:
+#         os.makedirs(app.instance_path)
+#     except OSError:
+#         pass
 
-    # a simple page that says hello
-    @app.route('/hello', methods=['POST'])
-    def hello():
-        jsdata = request.get_json()
-        print(jsdata)
+#     # a simple page that says hello
+#     @app.route('/hello', methods=['POST'])
+#     def hello():
+#         jsdata = request.get_json()
+#         print(jsdata)
 
-        y = []
+#         y = []
 
-        y = []
-        for item in jsdata:
-            y.append(int(jsdata[item]))
+#         y = []
+#         for item in jsdata:
+#             y.append(int(jsdata[item]))
 
-        array = numpy.array(y)
-        array = numpy.reshape(array, (1, 8))
+#         array = numpy.array(y)
+#         array = numpy.reshape(array, (1, 8))
     
 
-        loaded_model = pickle.load(open('app/saved_lin_reg.sav', 'rb'))
-        result = loaded_model.predict(array)
+#         loaded_model = pickle.load(open('app/saved_lin_reg.sav', 'rb'))
+#         result = loaded_model.predict(array)
 
         
      
-        return jsonify(result.tolist())
+#         return jsonify(result.tolist())
 
 
     
 
-    return app
+#     return app
+
+
+# app.py
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
